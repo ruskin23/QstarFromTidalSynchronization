7954a2863ef167cfc52678410bbfad0bb3175b82
Ruskin Patel <ruskinpatel@ruskins-mbp.home>
2018-08-29 12:29:09 -0500

    started mcmc_sampler

--- MCMC/mcmc_test.py
+++ MCMC/mcmc_test.py
@@ -5,18 +5,52 @@ pi=sc.pi
 
 class normal_distribution:
 
-    #Creates a normal distrubution given a value for mu and sigma
+    #Creates a normal distribution given a value for mu and sigma
 
     def __init__(self,mu,sigma):
         self.mu = mu
         self.sigma = sigma
 
-    def distribution(self, value):
+    def value_range(self):
+
+        n=5
+        min = self.mu - n*self.sigma
+        max = self.mu + n*self.sigma
+        v_array = sc.linspace(min, max, 1e+6)
+
+        return v_array
+
+
+    def distribution(self):
+
 
         A = 1/(self.sigma*sc.sqrt(2*pi))
-        B = -0.5*((value - self.mu)/self.sigma)**2
+        B = -0.5*(((self.value_range() - self.mu)/self.sigma)**2)
 
         return A*sc.exp(B)
 
 
 
+def proposal_function(mu_value,sigma_prior,sigma_step):
+
+    prior_function = normal_distribution(mu_value,sigma_prior).distribution()
+    step_function = normal_distribution(mu_value,sigma_step).distribution()
+
+    return prior_function*step_function
+
+#picking a random value from proposal function
+
+mu = 5668
+sigma = 0.2
+
+norm = normal_distribution(mu,sigma)
+
+x = norm.value_range()
+y = norm.distribution()
+
+
+print (x)
+print(y)
+
+plt.plot(x,y)
+plt.show()
