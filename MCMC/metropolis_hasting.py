5fb3029482700b10f380eaf39a4fd2e7f4e50220
Ruskin Patel <ruskin.patel23@gmail.com>
2018-09-19 11:46:55 -0500

    added articles

--- MCMC/metropolis_hasting.py
+++ MCMC/metropolis_hasting.py
@@ -1,113 +1,196 @@
 
+import sys
+sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/PythonPackage')
+sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/scripts')
+sys.path.append('/Users/ruskinpatel/Desktop/Research/QstarFromTidalSynchronization/')
+
+
+from binary_evolution.binary_evolution_class import binary_evolution
+from Calculate_Mass.Deriving_Mass import DerivePrimnaryMass
+from Calculate_Mass.Deriving_Mass import DeriveDeriveSecondaryMassMass
+
+import scipy
+from scipy.stats import norm
+import numpy
+
+
 class MetropolisHastings:
 
 
     def posterior_probability(self,parameter_set):
         """reutrns current surface spin of the star given the parameters"""
 
-        evolution = binary_evolution(interploator = self.interpolator, parameters = parameter_set,fixed_parameters = self.fixed_parameters)
-        model_spin = evolution()
+        evolution = binary_evolution(
+                                    interploator = self.interpolator,
+                                    observational_parameters = parameter_set,
+                                    fixed_parameters = self.fixed_parameters)
+
+        spin_given_model = evolution()
 
         prior = 1.0
 
         for (key_obs,value_obs),(key_parameter,value_parameter) in zip(self.observation_data.items(),parameter_set.items()):
             prior  *= scipy.stats.norm(value_obs['value'],value_obs['sigma']).pdf(value_parameter)
 
-        likelihood = scipy.stats.norm(self.observation_data['Pspin']['value'],self.observation_data['Pspin']['sigma']).pdf(model_spin)
+        likelihood = scipy.stats.norm(self.current_spin['value'],self.current_spin['sigma']).pdf(spin_given_model)
 
         posterior = prior*likelihood
 
         return posterior
 
+    def acceptance_probability(self):
 
-
-    def acceptance_probability(self,current_values,proposed_values):
-
-        p = self.posterior_probability(proposed_values)/self.posterior_probability(current_values)
+        posterior_proposed = self.posterior_probability(self.proposed_parameters)
+        posterior_previous = self.posterior_probability(self.updated_parameters)
+        p = posterior_proposed/posterior_previous
 
         return p
 
 
-    def values_proposed(self,current_values):
+    def values_proposed(self):
 
-        proposed = dict()
+        #propose values for observables
+        for (name_obs,value_obs),(name_step,value_step) in zip(self.updated_parameters.items(),self.proposed_step.items()):
+            proposed[name_obs]=scipy.stats.norm.rvs(loc=value_obs, scale=value_step)
 
-        for (name_obs,value_obs),(name_step,value_step) in zip(current_values.items(),self.proposed_step.items()):
+        #propose value for logQ
 
-            proposed[name_obs] =  scipy.stats.norm.rvs(loc=value_obs, scale=value_step)
 
-        return proposed
+    def write_file(self):
 
-    def metropolis_hasting(self):
+        data = []
+        data.append(self.iteration_step)
+        for value in self.updated_parameters.values():
+            data.append(value)
+
+
+        if self.isAccepted == True:
+            f = open('accepted_aparemters.txt')
+
+        else:
+            f = open('rejected_parameters.txt')
+
+        #f.write()
+        f.close()
+
+
+    def iterations(self):
 
         """runs the metropolis hastrings algorithm for number of iterations given"""
 
-        f.open("accpted_parameters.txt")
-        f.open("rejected_parameters.txt")
+        file1 = open('accepted_aparemters.txt')
+        file2 = open('rejected_parameters.txt'
+                     )
+        header = []
+        header.append('Iteration_step')
+        for key in self.updated_parameters.keys():
+            header.append(key)
 
-        # loop for number of iterations times
+
+        #file1.write(header)
+        #file2.write(header)
+
+        file.close()
+
+        if self.iteration_step==0:
+            self.updated_parameters = self.initial_parameters
+
 
         while self.iteration_step < self.total_iterations:
 
-            if self.iteration_step==0: current_values = self.start_values
-
             #draw a random value from proposal function
-            values_proposed = self.values_proposed(current_values)
+            self.proposed_parameters = self.values_proposed()
 
             # calculate acceptance probablity
-            p_acceptance = acceptance_probability(current_values, values_proposed)
+            p_acceptance = acceptance_probability()
 
             if p_acceptance > 1:
-
-                current_values = values_proposed
-                f.open("accepted_parameters.txt")
-                f.write('step = %d\t', iteration_step, 'value = %d', current_values, '\n')
-                f.close()
-
+                self.updated_parameters = self.proposed_parameters
+                self.isAccepted = True
             else:
+                rand = scipy.stats.norm.rvs()
+                if p_acceptance>rand: self.isAccepted=True
+                else:self.isAccepted=False
 
-                f.open("rejected_parameters.txt")
-                f.write('step = %d\t', iteration_step, 'value = %d', proposed_values, '\n')
-                f.close()
-
-                continue
-
-                self.iteration_step = self.iteration_step + 1
+            self.iteration_step = self.iteration_step + 1
 
         return none
 
 
-    def __init__(self,interpolator,fixed_parameters,observation_data,logQ,proposed_step,total_iterations):
+    def __init__(
+                self,
+                interpolator,
+                fixed_parameters,
+                observation_data,
+                logQ,
+                proposed_step,
+                total_iterations,
+                current_spin
+                ):
 
 
         self.interpolator  = interpolator
         self.fixed_parameters = fixed_parameters
         self.observation_data = observation_data
-        self.convective_phase_lag = phase_lag(logQ)
+        self.logQ = logQ
         self.proposed_step = proposed_step
         self.iteration_step = 0
         self.total_iterations= total_iterations
         self.updated_parameters = dict()
-        self.start_values = dict()
-
+        self.proposed_parameters = dict()
+        self.initial_parameters = dict()
+        for key, value in self.observation_data.items():
+            self.initial_parameters[key] = scipy.stats.norm.rvs(loc=key['value'], scale=key['sigma'])
+        self.isAccepted = None
+        self.current_spin = current_spin
 
 
 if __name__ == '__main__':
 
+    serialized_dir = "/Users/ruskinpatel/Desktop/Research/poet/stellar_evolution_interpolators"
+    manager = StellarEvolutionManager(serialized_dir)
+    interpolator = manager.get_interpolator_by_name('default')
 
-
-    observation_data = dict(Teff = dict(vlaue = 1.0, sigma = 0.1),
+    observation_data = dict(
+                        age = dict(vlaue = 1.0, sigma = 0.1),
+                        teff = dict(vlaue = 1.0, sigma = 0.1),
+                        Porb = dict(value = 1.0, sigma =0.1),
                         feh = dict(value = 1.0, sigma = 0.1),
                         rvk = dict(value = 1.0, sigma = 0.1),
-                        inclination = dict(value = 1.0, sigma = 0.1)
+                        inclination = dict(value = 1.0, sigma = 0.1),
                     )
 
-    fixed_parameters = dict(disk_dissipation_age =  dict(value = 1.0, sigma = 0.1))
+    current_spin = dict(
+                        value = 1.0,
+                        sigma = 0.1
+                        )
 
-    proposed_step = dict( Teff_step = 0.1,
-                      feh_step = 0.1,
-                      rvk_step = 0.1,
-                      inclination = 0.1)
+    fixed_parameters = dict(
+                        disk_dissipation_age = 1.0,
+                        disk_lock_frequency = 1.0,
+                        wind = True,
+                        wind_saturation_frequency = 1.0,
+                        diff_rot_coupling_timescale = 1.0
+                    )
+
+    proposed_step = dict(
+                        age_step = 0.1,
+                        teff_step = 0.1,
+                        Porb_step = 0.1,
+                        feh_step = 0.1,
+                        rvk_step = 0.1,
+                        inclination = 0.1,
+                    )
+
+
+    logQ = dict(
+                min = 4,
+                max = 6,
+                step = 0.1)
 
 
 
+
+#flush()
+#buffer_size =0 to write at the moment
+#
