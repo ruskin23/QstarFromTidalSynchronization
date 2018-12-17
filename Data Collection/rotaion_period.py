6d885b4b3c1b3b4d315aff75d5f800d47fe0fb15
Ruskin Patel <ruskin.patel23@gmail.com>
2018-12-17 16:24:04 -0600

    idata

--- Data Collection/rotaion_period.py
+++ Data Collection/rotaion_period.py
@@ -1,23 +1,14 @@
-#import matplotlib.pyplot as plt
-#from astropy.visualization import astropy_mpl_style
-#plt.style.use(astropy_mpl_style)
 
-#from astropy.utils.data import get_pkg_data_filename
-#from astropy.io import fits
-
-#image_file = 'kepler_EB_test1.fits'
+from astropy.io import fits
+from matplotlib import pyplot
 
 
-#fits.info(image_file)
-#image_data = fits.getdata(image_file, ext=0)
-#print(image_data.shape)
+image_file = 'kepler_EB_test1.fits'
 
 
-#plt.figure()
-#plt.imshow(image_data, cmap='gray')
-#plt.colorbar()
+fits.info(image_file)
+image_data = fits.getdata(image_file, ext=0)
 
 
-
-
-
+plt.figure()
+plt.imshow(image_data, cmap='gray')
