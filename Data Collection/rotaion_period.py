72396a7423eab74d0b82456951d04e2dcde34019
Ruskin Patel <ruskin.patel23@gmail.com>
2018-12-18 16:17:02 -0600

    peak frequency Please enter the commit message for your changes. Lines starting

--- Data Collection/rotaion_period.py
+++ Data Collection/rotaion_period.py
@@ -1,14 +1,50 @@
+from matplotlib import pyplot
+import numpy
 
 from astropy.io import fits
-from matplotlib import pyplot
+from astropy.stats import LombScargle
+
+if __name__ == '__main__':
+    eb_lc = fits.open('KIC7289157.fits')
+    time = eb_lc[1].data['TIME'] - 1
+    flux = eb_lc[1].data['PDCSAP_FLUX']
+    period = 5.266273
+
+    in_eclipse = numpy.logical_or(
+        numpy.logical_and(
+            time % period > 1.45,
+            time % period < 1.70,
+        ),
+        numpy.logical_and(
+            time % period > 4.15,
+            time % period < 4.44,
+        )
+    )
+
+    out_of_eclipse = numpy.logical_and(
+        numpy.isfinite(flux),
+        numpy.logical_not(in_eclipse)
+    )
+
+    non_nan = numpy.isfinite(flux)
 
 
-image_file = 'kepler_EB_test1.fits'
 
+    frequency_ooe, power_ooe = LombScargle(time[out_of_eclipse],
+                                           flux[out_of_eclipse]).autopower()
 
-fits.info(image_file)
-image_data = fits.getdata(image_file, ext=0)
+    freq_min = 0.06
+    freq_max = 0.2
 
+    frequency_range = numpy.logical_and(
+        frequency_ooe > freq_min,
+        frequency_ooe < freq_max
+    )
 
-plt.figure()
-plt.imshow(image_data, cmap='gray')
+    new_frequency, new_power = frequency_ooe[frequency_range],power_ooe[frequency_range]
+
+    peak_frequency = new_frequency[numpy.argmax(new_power)]
+
+    spin_period = 1.0/peak_frequency
+
+    print (spin_period)
