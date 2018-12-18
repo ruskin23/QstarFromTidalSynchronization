52b5073ef409f857e34ffa2210c0535c5c974685
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2018-12-18 15:09:01 -0600

    More thorough version of the rotation detection script.

--- Data Collection/check_eb_spin.py
+++ Data Collection/check_eb_spin.py
@@ -5,18 +5,19 @@ from astropy.io import fits
 from astropy.stats import LombScargle
 
 if __name__ == '__main__':
-    eb_lc = fits.open('kepler_EB_test1.fits')
-    time = eb_lc[1].data['TIME']
-    flux = eb_lc[1].data['LC_DETREND']
-    period = 2.0 * eb_lc[1].header['TPERIOD']
+    eb_lc = fits.open('KIC7289157.fits')
+    time = eb_lc[1].data['TIME'] - 1
+    flux = eb_lc[1].data['PDCSAP_FLUX']
+    period = 5.266273
+
     in_eclipse = numpy.logical_or(
         numpy.logical_and(
-            time % period > 0.92,
-            time % period < 1.24,
+            time % period > 1.45,
+            time % period < 1.70,
         ),
         numpy.logical_and(
-            time % period > 3.13,
-            time % period < 3.45,
+            time % period > 4.15,
+            time % period < 4.44,
         )
     )
     out_of_eclipse = numpy.logical_and(
@@ -25,15 +26,33 @@ if __name__ == '__main__':
     )
     non_nan = numpy.isfinite(flux)
 
+    pyplot.plot(time[out_of_eclipse],
+                flux[out_of_eclipse],
+                '.g')
+
+    pyplot.plot(time[in_eclipse],
+                flux[in_eclipse],
+                '.r')
+    pyplot.show()
+
     frequency_all, power_all = LombScargle(time[non_nan],
                                            flux[non_nan]).autopower()
-#    pyplot.plot(frequency_all, power_all, '-g')
+    pyplot.semilogx(1.0 / frequency_all, power_all, '-g')
 
     frequency_ooe, power_ooe = LombScargle(time[out_of_eclipse],
                                            flux[out_of_eclipse]).autopower()
     pyplot.semilogx(1.0 / frequency_ooe, power_ooe, '-r')
-    pyplot.axvline(eb_lc[1].header['TPERIOD'])
+    pyplot.axvline(period)
     pyplot.show()
 
-    pyplot.plot(time[out_of_eclipse] % period, flux[out_of_eclipse], '.g')
+    period = 7.75
+
+    num_in_chunk = out_of_eclipse.sum() // 5
+    for chunk, color in zip(range(5), 'rgbcm'):
+        selected = numpy.copy(out_of_eclipse)
+        selected[0: num_in_chunk * chunk] = False
+        selected[num_in_chunk * (chunk + 1):] = False
+        pyplot.plot(time[selected] % period,
+                    flux[selected],
+                    '.' + color)
     pyplot.show()
