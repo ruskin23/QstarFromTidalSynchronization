68b276ce56fb8a862c8140e758fb57906775de69
Ruskin Patel <ruskin.patel23@gmail.com>
2018-12-22 14:37:40 -0600

    edited mass calculations to allow if only semimajor axis is known

--- Calculate_Mass/mass_semimajor.py
+++ Calculate_Mass/mass_semimajor.py
@@ -1,8 +1,10 @@
-"""Allows finding mass given other age at fixed [Fe/H]."""
+"""Calculate primary and secondary mass if the age, [Fe/H] and semimajor axis are known """
+
+
 import sys
+
 sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/PythonPackage')
 sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/scripts')
-
 
 from stellar_evolution.manager import StellarEvolutionManager
 from stellar_evolution.derived_stellar_quantities import TeffK
@@ -11,7 +13,9 @@ import scipy.interpolate
 import scipy.linalg
 import scipy.optimize
 
-G=1534949.0910000005;
+G = 1534949.0910000005;
+constant = 0.0244e7
+
 
 class DerivePrimnaryMass:
 
@@ -23,43 +27,37 @@ class DerivePrimnaryMass:
         self.age = age
         self.teff_value = teff_value
 
-
     def teff_diff(self, mass):
 
         """Return the effective temperature for the given stellar mass."""
 
         T = TeffK(self.interp('radius', mass, self.feh),
                   self.interp('lum', mass, self.feh)
-                 )
+                  )
 
-        return (T(self.age)-self.teff_value)
-
+        return (T(self.age) - self.teff_value)
 
     def possible_solution(self):
 
         """Finds 2 values of masses between which a solution is present"""
 
-
-        mass_array = scipy.linspace(0.4,1.2,100)
+        mass_array = scipy.linspace(0.4, 1.2, 100)
         teff_array_diff = scipy.empty(len(mass_array))
 
-        for m_index,m_value in enumerate(mass_array):
-
+        for m_index, m_value in enumerate(mass_array):
             teff_array_diff[m_index] = self.teff_diff(m_value)
 
         mass_solutions = []
 
         for i in range(teff_array_diff.size):
 
-            x = teff_array_diff[i]*teff_array_diff[i+1]
-            if x<0:
+            x = teff_array_diff[i] * teff_array_diff[i + 1]
+            if x < 0:
                 mass_solutions.append(mass_array[i])
-                mass_solutions.append(mass_array[i+1])
+                mass_solutions.append(mass_array[i + 1])
                 break
 
-
         return mass_solutions
-
 
     def __call__(self):
 
@@ -69,52 +67,25 @@ class DerivePrimnaryMass:
 
         solution = scipy.optimize.brentq(self.teff_diff, mass_solutions[0], mass_solutions[1])
 
+        print ('PRIMARY MASS = ', solution)
+
+
         return solution
 
 
 class DeriveSecondaryMass:
 
+    def __init__(self,period,semimajor,mass_primary):
 
-    def __init__(self, period, semimajor, mass_primary ):
-	
-        self.period = period
+        self.period  = period
         self.semimajor = semimajor
         self.mass_primary = mass_primary
 
-    def mass_function(self,mass_secondary):
-
-        return (mass_secondary**3)/(mass_secondary + self.mass_primary)**2
-
-    def difference_function(self, mass_secondary):
-
-        return self.mass_function(mass_secondary) - (((4*scipy.pi)**2)*(self.semimajor**3))/((self.period**2)*G)
 
     def __call__(self):
 
-        mass_array = scipy.linspace(0.4,1.2,100)
-        differnece_array = scipy.empty(len(mass_array))
+        solution = (constant * ((self.semimajor) ** 3)) / ((self.period) ** 2) - self.mass_primary
 
-        for m_index, m_value in enumerate(mass_array):
-            differnece_array[m_index] = self.difference_function(m_value)
-
-        mass_solutions = []
-
-        for i in range(differnece_array.size):
-
-            x = differnece_array[i]*differnece_array[i+1]
-            if x<0:
-                mass_solutions.append(mass_array[i])
-                mass_solutions.append(mass_array[i+1])
-                break
-
-        solution = scipy.optimize.brentq(self.difference_function, mass_solutions[0], mass_solutions[1])
+        print ('SECONDARY MASS = ', solution)
 
         return solution
-
-
-serialized_dir = "/Users/ruskinpatel/Desktop/Research/poet/stellar_evolution_interpolators"
-manager = StellarEvolutionManager(serialized_dir)
-interpolator = manager.get_interpolator_by_name('default')
-
-x=DerivePrimnaryMass(interpolator, -0.06, 4.6, 5922)
-print(x())
