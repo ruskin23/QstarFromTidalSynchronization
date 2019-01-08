02466e19c13468914d9a56307b5fbc73193a0ed2
Ruskin <ruskin.patel23@gmail.com>
2019-01-08 15:09:08 -0600

    single mcmc running

--- MCMC/mass_calculations.py
+++ MCMC/mass_calculations.py
@@ -3,12 +3,11 @@
 
 import sys
 
-sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/PythonPackage')
-sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/scripts')
+#sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/PythonPackage')
+#sys.path.append('/Users/ruskinpatel/Desktop/Research/poet/scripts')
 
-#import sys
-#sys.path.append('/home/kpenev/projects/git/poet/PythonPackage')
-#sys.path.append('/home/kpenev/projects/git/poet/scripts')
+sys.path.append('/home/kpenev/projects/git/poet/PythonPackage')
+sys.path.append('/home/kpenev/projects/git/poet/scripts')
 
 from stellar_evolution.manager import StellarEvolutionManager
 from stellar_evolution.derived_stellar_quantities import TeffK
@@ -35,11 +34,17 @@ class DerivePrimnaryMass:
 
         """Return the effective temperature for the given stellar mass."""
 
+
+
         T = TeffK(self.interp('radius', mass, self.feh),
                   self.interp('lum', mass, self.feh)
                   )
 
-        return (T(self.age) - self.teff_value)
+        if self.age >T.min_age and self.age<T.max_age:
+
+            return (T(self.age) - self.teff_value)
+
+        else: return scipy.nan
 
     def possible_solution(self):
 
@@ -49,12 +54,13 @@ class DerivePrimnaryMass:
         teff_array_diff = scipy.empty(len(mass_array))
 
         for m_index, m_value in enumerate(mass_array):
+
             teff_array_diff[m_index] = self.teff_diff(m_value)
+
 
         mass_solutions = []
 
         for i in range(teff_array_diff.size):
-
             x = teff_array_diff[i] * teff_array_diff[i + 1]
             if x < 0:
                 mass_solutions.append(mass_array[i])
@@ -67,6 +73,7 @@ class DerivePrimnaryMass:
 
         """Optimize the possible solution to calculate exact solution"""
 
+
         mass_solutions = self.possible_solution()
 
         solution = scipy.optimize.brentq(self.teff_diff, mass_solutions[0], mass_solutions[1])
@@ -77,19 +84,59 @@ class DerivePrimnaryMass:
 
 class DeriveSecondaryMass:
 
-    def __init__(self,period,semimajor,mass_primary):
+    def __init__(self, interpolator, feh, age, teff_value):
+        """Set-up to use the given interpolator."""
 
-        self.period  = period
-        self.semimajor = semimajor
-        self.mass_primary = mass_primary
+        self.interp = interpolator
+        self.feh = feh
+        self.age = age
+        self.teff_value = teff_value
+
+
+    def teff_diff(self, mass):
+        """Return the effective temperature for the given stellar mass."""
+
+        T = TeffK(self.interp('radius', mass, self.feh),
+                  self.interp('lum', mass, self.feh)
+               )
+
+        if self.age > T.min_age and self.age < T.max_age:
+
+            return (T(self.age) - self.teff_value)
+
+        else:
+            return scipy.nan
+
+
+    def possible_solution(self):
+        """Finds 2 values of masses between which a solution is present"""
+
+        mass_array = scipy.linspace(0.4, 1.2, 100)
+        teff_array_diff = scipy.empty(len(mass_array))
+
+        for m_index, m_value in enumerate(mass_array):
+
+            teff_array_diff[m_index] = self.teff_diff(m_value)
+
+        mass_solutions = []
+
+
+        for i in range(teff_array_diff.size):
+
+            x = teff_array_diff[i] * teff_array_diff[i + 1]
+            if x < 0:
+                mass_solutions.append(mass_array[i])
+                mass_solutions.append(mass_array[i + 1])
+                break
+
+        return mass_solutions
 
 
     def __call__(self):
+        """Optimize the possible solution to calculate exact solution"""
 
-        solution = (constant * ((self.semimajor) ** 3)) / ((self.period) ** 2) - self.mass_primary
+        mass_solutions = self.possible_solution()
 
-        print ("period = ", self.period)
-        print ("semimajor = ", self.semimajor)
-
+        solution = scipy.optimize.brentq(self.teff_diff, mass_solutions[0], mass_solutions[1])
 
         return solution
