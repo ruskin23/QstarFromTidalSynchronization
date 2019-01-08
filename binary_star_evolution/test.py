764b7298b9087bf26af1941b454eaba80bc6cfbb
Ruskin Patel <ruskin.patel23@gmail.com>
2019-01-08 17:17:22 -0600

    checking evolution on my pc

---
+++ binary_star_evolution/test.py
@@ -0,0 +1,9 @@
+quantities = ['primary_envelope', 'primary_core', 'secondary_envelope', 'secondary_core']
+
+print(' '.join(['%30s' % q for q in quantities]), end=' ')
+
+for i in range(10):
+    print(' '.join(['%30s' % i, q)[i])
+    for q in quantities]), end=' ')
+
+
