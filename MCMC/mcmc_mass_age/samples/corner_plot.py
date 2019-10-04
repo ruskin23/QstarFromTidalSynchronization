2ed8201d46237a95359b6382093e9e9daca11997
Ruskin <ruskin.patel23@gmail.com>
2019-10-04 18:37:22 -0500

    Corrected the file names for samples

--- MCMC/mcmc_mass_age/samples/corner_plot.py
+++ MCMC/mcmc_mass_age/samples/corner_plot.py
@@ -12,24 +12,12 @@ args=parser.parse_args()
 system=args.system
 
 data=[]
-mass=[]
-age=[]
-feh=[]
-
-with open('nominal_values','w') as f1:
-    f1.write('system'+'\t'+
-             'mass'+'\t'+
-             'age'+'\t'+
-             'feh'+'\n')
 
 with open('mass_age_teff_sample_'+system+'.txt','r') as f:
     next(f)
     for lines in f:
         x=lines.split()
         try:
-            mass.append(float(x[1]))
-            age.append(float(x[2]))
-            feh.append(float(x[3]))
             data.append(float(x[1]))
             data.append(float(x[2]))
             data.append(float(x[3]))
