7e1be3f7103845d9fdbd171f79a6ed55b8b30437
Ruskin <ruskin.patel23@gmail.com>
2020-02-27 20:26:26 -0600

    updated cluster files to conitnue mcmc runs

---
+++ cluster_files/stampede2/MCMC/WriteJobs.sh
@@ -0,0 +1,42 @@
+for A in 85 76 96 81 80 36 83 84 94
+do
+	for B in 1 2 3 4 5 
+	do
+		echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/$A.$B.'$LAUNCHER_TSK_ID.out' 
+	done
+done >NonSyncJob1
+
+for A in 32 106 123 50 39 56 126 54 70 
+do
+    for B in 1 2 3 4 5
+    do
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/$A.$B.'$LAUNCHER_TSK_ID.out' 
+    done
+done >NonSyncJob2
+
+for A in 88 67 95 25 137 1 86 43
+do
+    for B in 1 2 3 4 5
+    do
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/$A.$B.'$LAUNCHER_TSK_ID.out' 
+    done
+done >NonSyncJob3
+
+
+for A in 73 92 93 79 47 109 44 48 
+do
+    for B in 1 2 3 4 5
+    do
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/$A.$B.'$LAUNCHER_TSK_ID.out' 
+    done
+done >SyncJob1
+
+for A in 17 8 12 20 31 57 120 28 13
+do
+    for B in 1 2 3 4 5
+    do
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/$A.$B.'$LAUNCHER_TSK_ID.out' 
+    done
+done >SyncJob2
+
+
