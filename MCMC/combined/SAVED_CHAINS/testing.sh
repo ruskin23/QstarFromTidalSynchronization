cea8faf7495abdf2b11cee9e460e5b8436f44fc4
Ruskin <ruskin.patel23@gmail.com>
2020-03-31 03:13:48 -0500

    updated latex doc

--- MCMC/combined/SAVED_CHAINS/testing.sh
+++ MCMC/combined/SAVED_CHAINS/testing.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm temp.tex
+
 echo "\documentclass[10pt]{article}
 \usepackage{algorithmic}
 \usepackage{algorithm}
@@ -19,8 +21,6 @@ echo "\documentclass[10pt]{article}
 
 for S in 85 76 96 81 80 36 83 84 94 32 106 123 50 39 56 126 54 70 88 67 95 25 137 1 86 43 73 92 93 79 47 109 44 48 17 8 12 20 31 57 120 28 13
     do
-        ./corner_plot.sh $S
-
 		cat ganymede/MCMC_$S/accepted_parameters_*.txt >ganymede/MCMC_$S/combined_accepted.txt
         cat stampede/MCMC_$S/accepted_parameters_*.txt >stampede/MCMC_$S/combined_accepted.txt
 		cat ganymede/MCMC_$S/combined_accepted.txt stampede/MCMC_$S/combined_accepted.txt >combined_accepted.txt
@@ -44,16 +44,28 @@ for S in 85 76 96 81 80 36 83 84 94 32 106 123 50 39 56 126 54 70 88 67 95 25 13
         r=$(bc <<<"scale=2;$A/$sum")
         
         T="System=$S|Accepted=${A}|Rejected=${R}|AcceptanceRatio=$r"
-	    echo "$T" >> temp.tex
-    
-        echo "\begin{figure}[h]" >> temp.tex 
-        echo "\includegraphics[width=15cm]{CornerPlot_$S.eps}" >> temp.tex
-        echo "\caption{$T}" >> temp.tex
-        echo "\label{S$S}" >> temp.tex
-        echo "\centering" >> temp.tex
-        echo "\end{figure}" >> temp.tex
-        echo "" >> temp.tex
+       
+        b="\\"
+        
+        echo "\begin{center}
+        \begin{tabular}{|c|c|c|}
+        \hline" >> temp.tex
+
+        printf 'Orbital Period & Spin Period & $\sigma_{spin}$ %s%s\n ' "$b$b" >> temp.tex
+
+        echo "\hline
+        \end{tabular}
+        \end{center}" >> temp.tex
+        
+        echo "\begin{figure}[h] 
+        \includegraphics[width=15cm]{CornerPlot_$S.eps}
+        \caption{$T}
+        \label{S$S}
+        \centering
+        \end{figure}" >> temp.tex
 
 
 
 done
+
+echo "\end{document}" >>temp.tex
