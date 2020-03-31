b7603826fdd9d9a160d0d6e080106dbf017d2ba7
Ruskin <ruskin.patel23@gmail.com>
2020-03-30 23:08:16 -0500

    updating slurm scripts to run in stampede

--- cluster_files/stampede2/MCMC/WriteJobs.sh
+++ cluster_files/stampede2/MCMC/WriteJobs.sh
@@ -1,8 +1,8 @@
-for A in 85 76 96 81 80 36 83 84 94
+for A in 85 76 96 81 80 36 83 94
 do
 	for B in 1 2 3 4 5 
 	do
-		echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/fifth_run/$A.$B.out
+		echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out
 	done
 done >NonSyncJob1
 
@@ -10,24 +10,29 @@ for A in 32 106 123 50 39 56 126 54 70
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/fifth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
     done
 done >NonSyncJob2
 
-for A in 88 67 95 25 137 1 86 43
+for A in 88 67 95 25 137 86 43
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/fifth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
     done
-done >NonSyncJob3
-
+done >tmp1
+for A in 1 2 3 4 5
+do
+    echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $A -l 1 -m adaptive >"/scratch/06850/rpatel23/output/sixth_run/1.$A.out
+done >tmp2
+cat tmp1 tmp2 >NonSyncJob3
+rm tmp1 tmp2
 
 for A in 73 92 93 79 47 109 44 48 
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/fifth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
     done
 done >SyncJob1
 
@@ -35,7 +40,7 @@ for A in 17 8 12 20 31 57 120 28 13
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A >"/scratch/06850/rpatel23/output/fifth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
     done
 done >SyncJob2
 
