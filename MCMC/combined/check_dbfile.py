e19f544d23f2778a736d6663a22fd6ab004ab7f4
Ruskin <ruskin.patel23@gmail.com>
2020-04-11 14:20:43 -0500

    testing failure of covarince db file on lcuster

--- MCMC/combined/check_dbfile.py
+++ MCMC/combined/check_dbfile.py
@@ -6,7 +6,7 @@ work_dir='/work/06850/rpatel23/stampede2'
 
 current_directory=work_dir+git_dir
 
-s=shelve.open('covariance.db')
+s=shelve.open(current_directory+'/covariance.db')
 covariance_matrix=s['81']
 s.close()
 
