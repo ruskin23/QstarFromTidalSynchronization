7d45bb81fdce512c5fc7f57cd8f6598ca411c00f
Ruskin <ruskin.patel23@gmail.com>
2020-04-28 02:16:17 -0500

    updated slurm scripts for net runs

--- cluster_files/stampede2/MCMC/v1.2/WriteJobs.sh
+++ cluster_files/stampede2/MCMC/v1.2/WriteJobs.sh
@@ -2,7 +2,7 @@ for A in 17 25 109 120 83 106 57 36 88
 do
 	for B in 1 2 3 4 5 
 	do
-		echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out
+		echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out
 	done
 done >uncorrelated1
 
@@ -10,7 +10,7 @@ for A in 44 123 28 47 79 94 13 48 43
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out 
     done
 done >uncorrelated2
 
@@ -18,7 +18,7 @@ for A in 93 70 8 12 31
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out 
     done
 done >common
 #done >uncorrelated3
@@ -27,7 +27,7 @@ done >common
 #do
 #    for B in 1 2 3 4 5
 #    do
-#        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -p $B -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out >> uncorrelated3
+#        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -p $B -m uncorrelated >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out >> uncorrelated3
 #    done
 #done 
 #
@@ -35,7 +35,7 @@ done >common
 #do
 #    for B in 1 2 3 4 5
 #    do
-#        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -p $B -m uncorrelated >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
+#        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -p $B -m uncorrelated >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out 
 #    done
 #done >uncorrelated4
 
@@ -44,7 +44,7 @@ for A in 39 137 54 80 126 76 1 32 67
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out 
     done
 done >adaptive1
 
@@ -52,7 +52,7 @@ for A in 81 95 96 50 85 56 73 86 92
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out 
     done
 done >adaptive2
 
@@ -60,7 +60,7 @@ for A in 20
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/sixth_run/$A.$B.out  >> common        
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/seventh_run/$A.$B.out  >> common        
     done
 done
 #done >adaptive3
