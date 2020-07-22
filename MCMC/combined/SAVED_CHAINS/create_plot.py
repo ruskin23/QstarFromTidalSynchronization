fc4662b9589e9277f2c24d86416cd634d6cd546c
Ruskin <ruskin.patel23@gmail.com>
2020-07-22 14:00:15 -0500

    saving chains

--- MCMC/combined/SAVED_CHAINS/create_plot.py
+++ MCMC/combined/SAVED_CHAINS/create_plot.py
@@ -11,18 +11,18 @@ with open('limits.txt','r') as f:
     for lines in f:
         x=lines.split()
         values=[float(x[1]),float(x[2]),float(x[3])]
-        if float(x[6])==0:
-            tf=[float(x[7]),float(x[7]),float(x[7])]
-            c='b'
-            plt.semilogx(tf,values,'-o',color=c)
-            x_direct=1
-            y_direct=0
-            plt.quiver(float(x[7]),float(x[1]),x_direct,y_direct,color='k',width=0.002)
+        #if float(x[6])==0:
+        tf=[float(x[8]),float(x[8]),float(x[8])]
+        c='b'
+        #plt.semilogx(tf,values,'-o',color=c)
+        #x_direct=1
+        #y_direct=0
+        #plt.quiver(float(x[7]),float(x[1]),x_direct,y_direct,color='k',width=0.002)
 
-            continue
-        if x[0] in good_systems:c='r'
-        else:c='g'
-        tf=[2*numpy.pi/float(x[6]),2*numpy.pi/float(x[6]),2*numpy.pi/float(x[6])]
+        #continue
+        #if x[0] in good_systems:c='r'
+        #else:c='g'
+        #tf=[2*numpy.pi/float(x[6]),2*numpy.pi/float(x[6]),2*numpy.pi/float(x[6])]
         plt.semilogx(tf,values,'-o',color=c)
 
 plt.show()
