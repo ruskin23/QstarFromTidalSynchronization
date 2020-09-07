7b5a312669d1285aaf38b439762ba4299b6b3d9b
Ruskin <ruskin.patel23@gmail.com>
2020-09-07 16:44:55 -0500

    updated ganymede ands stampede slurm scripts for Run22 and Run24

--- cluster_files/ganymede/MCMC/v1.2/WriteJobs.sh
+++ cluster_files/ganymede/MCMC/v1.2/WriteJobs.sh
@@ -3,10 +3,10 @@
 
 for B in 1 2 3 4 5
 do
-    echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l 1 -m adaptive >/home/rxp163130/scratch/Run21/1.$B.out"
+    echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l 1 -m adaptive >/home/rxp163130/scratch/Run22/1.$B.out"
 	for A in 39 137 54 80 126 76 1 32 67 81 95 96 85 56 73 86 92 20
 	do
-		echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A -m adaptive >/home/rxp163130/scratch/Run21/$A.$B.out"
+		echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A -m adaptive >/home/rxp163130/scratch/Run22/$A.$B.out"
 	done
 done >adaptive
 
@@ -16,7 +16,7 @@ for B in 1 2 3 4 5
 do
     for A in 17 83 57 36 88 44 94 13 48 43 93 70 31
     do
-        echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A -m uncorrelated >/home/rxp163130/scratch/Run21/$A.$B.out"
+        echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A -m uncorrelated >/home/rxp163130/scratch/Run22/$A.$B.out"
     done
 done >uncorrelated
 
