eaf4759a33f0de56743a72ed975537b886722044
Ruskin Patel <rxp163130@ganymede.utdallas.edu>
2020-10-12 22:17:40 -0500

    changed -c to -s in jobfile for MCMC76

--- cluster_files/ganymede/MCMC/test_solver/Writejob.sh
+++ cluster_files/ganymede/MCMC/test_solver/Writejob.sh
@@ -4,5 +4,5 @@
 
 for I in {11..20}
 do
-    echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $I -l 76 -m uncorrelated >/home/rxp163130/scratch/test_solver/76.$I.out"
+    echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/combined/main.py -s -i $I -l 76 -m uncorrelated >/home/rxp163130/scratch/test_solver/76.$I.out"
 done >jobfile
