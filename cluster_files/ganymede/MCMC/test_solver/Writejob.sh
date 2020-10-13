43015c0cef940508b4a98e493fc1b40bfc9701a1
Ruskin <ruskin.patel23@gmail.com>
2020-10-12 22:06:51 -0500

    fixes output error of test_slurm script

--- cluster_files/ganymede/MCMC/test_solver/Writejob.sh
+++ cluster_files/ganymede/MCMC/test_solver/Writejob.sh
@@ -4,5 +4,5 @@
 
 for I in {1..15}
 do
-    echo "./main.py -c -i $I -l 76 -m uncorrelated >/home/rxp163130/scratch/test_solver/$A.$B.out"
+    echo "./main.py -c -i $I -l 76 -m uncorrelated >/home/rxp163130/scratch/test_solver/76.$I.out"
 done >jobfile
