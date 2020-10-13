700c96b60ec9c88cd0ba17e8860874d135f83dc9
Ruskin <ruskin.patel23@gmail.com>
2020-10-13 18:09:02 -0500

    updated slurm scripts, ganymede Run27, stampede Run29

--- cluster_files/stampede2/MCMC/v1.2/WriteJobs.sh
+++ cluster_files/stampede2/MCMC/v1.2/WriteJobs.sh
@@ -2,7 +2,7 @@ for A in 17 83 57 36 88
 do
 	for B in 1 2 3 4 5 
 	do
-		echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/Run28/$A.$B.out
+		echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/Run29/$A.$B.out
 	done
 done >uncorrelated1
 
@@ -10,7 +10,7 @@ for A in 44 94 13 48 43 93
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/Run28/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m uncorrelated >"/scratch/06850/rpatel23/output/Run29/$A.$B.out 
     done
 done >uncorrelated2
 
@@ -18,7 +18,7 @@ for A in 39 137 54 80 126 76 1 32 67
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/Run28/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/Run29/$A.$B.out 
     done
 done >adaptive1
 
@@ -26,7 +26,7 @@ for A in 81 95 96 85 56 73 86 92 20
 do
     for B in 1 2 3 4 5
     do
-        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/Run28/$A.$B.out 
+        echo "python3 /work/06850/rpatel23/stampede2/QstarFromTidalSynchronization/MCMC/combined/main.py -c -i $B -l $A  -m adaptive >"/scratch/06850/rpatel23/output/Run29/$A.$B.out 
     done
 done >adaptive2
 
