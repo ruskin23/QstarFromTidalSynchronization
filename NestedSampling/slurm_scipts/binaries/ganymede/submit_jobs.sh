7a2b80ffad31d525afc7b809cf3f259db0f5c677
Ruskin <ruskin.patel23@gmail.com>
2020-10-16 14:18:56 -0500

    updated submitjob script to allow different systems

--- NestedSampling/slurm_scipts/binaries/ganymede/submit_jobs.sh
+++ NestedSampling/slurm_scipts/binaries/ganymede/submit_jobs.sh
@@ -1,4 +1,4 @@
 for S in 39,54,76,80,81,92,126
 do
-    sbatch  --job-name=nlive.$S --output=/home/rxp163130/scratch/Nested_Sampling/slurm_output.nlive_init.$S.ounlive_init.slurm --export=ALL,S=$S nlive_init.slurm
+    sbatch nlive_init_$S.slurm
 done
