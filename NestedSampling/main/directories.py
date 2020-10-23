bfba6222fbf445c3cfca3c71172bdcc6bbc67207
Ruskin Patel <rxp163130@ganymede.utdallas.edu>
2020-10-23 17:13:40 -0500

    modified directories to have correct path

--- NestedSampling/main/directories.py
+++ NestedSampling/main/directories.py
@@ -7,17 +7,17 @@ class directories:
         if home_dir=='/home/ruskin':
             self.poet_path=home_dir+'/projects/poet'
             self.current_directory=home_dir+'/projects'+self.git_dir
-            self.output_directory=self.current_directory+'/Cluster_Results/kartof'
+            self.output_directory=self.current_directory+'/results/kartof'
 
         if home_dir=='/home/rxp163130':
             self.poet_path=home_dir+'/poet'
             self.current_directory=home_dir+self.git_dir
-            self.output_directory=self.current_directory+'/Cluster_Results/ganymede'
+            self.output_directory=self.current_directory+'/results/ganymede'
 
         if home_dir=='/home1/06850/rpatel23':
             self.work_dir='/work/06850/rpatel23/stampede2'
             self.poet_path=self.work_dir+'/poet'
             self.current_directory=self.work_dir+self.git_dir
-            self.output_directory=self.current_directory+'/Cluster_Results/stampede'
+            self.output_directory=self.current_directory+'/results/stampede'
 
 
