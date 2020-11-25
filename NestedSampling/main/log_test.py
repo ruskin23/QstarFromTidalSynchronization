2f37e1fd27c673f2b87a0e57d9b1cb4b473500b6
Ruskin <ruskin.patel23@gmail.com>
2020-11-25 14:35:18 -0600

    updated plottinh script of saved sample_init object

--- NestedSampling/main/log_test.py
+++ NestedSampling/main/log_test.py
@@ -2,20 +2,33 @@
 
 import logging
 
+def add_handler(logger):
+
+    # Create handlers
+    f_handler = logging.FileHandler('file.log',mode='a')
+
+    # Create formatters and add it to handlers
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    f_handler.setFormatter(f_format)
+
+    # Add handlers to the logger
+    logger.addHandler(f_handler)
+
+    logger.warning('This is a warning')
+    logger.error('This is an error')
+    logger.info('this is info rofl')
+
+
+def remove_handler(logger):
+
+    logger.removeHandler(logging.getLogger().handlers)
+
 # Create a custom logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-# Create handlers
-f_handler = logging.FileHandler('file.log',mode='w')
 # f_handler.setLevel(logging.DEBUG)
 
-# Create formatters and add it to handlers
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-f_handler.setFormatter(f_format)
+add_handler(logger)
+remove_handler(logger)
+add_handler(logger)
 
-# Add handlers to the logger
-logger.addHandler(f_handler)
-
-logger.warning('This is a warning')
-logger.error('This is an error')
-logger.info('this is info rofl')
