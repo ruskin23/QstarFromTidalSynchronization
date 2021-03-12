8dfe7223a81a50116cc04b7e4eb5016ec5ca6ea9
Ruskin <ruskin.patel23@gmail.com>
2021-03-12 11:51:57 -0600

    updating slurm scripts for ganymede Run43 stampede Run45

--- cluster_files/ganymede/MCMC/v1.2/WriteJobs.sh
+++ cluster_files/ganymede/MCMC/v1.2/WriteJobs.sh
@@ -5,7 +5,7 @@ for B in 1 2 3 4 5
 do
 	for A in 20 50 39 137 54 80 126 76 1 32 67 81 95 96 85 56 73 86 92
 	do
-		echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/version1/main.py -c -i $B -l $A -m adaptive >/home/rxp163130/scratch/Run42/$A.$B.out"
+		echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/version1/main.py -c -i $B -l $A -m adaptive >/home/rxp163130/scratch/Run43/$A.$B.out"
 	done
 done >adaptive
 
@@ -15,7 +15,7 @@ for B in 1 2 3 4 5
 do
     for A in 12 17 47 106 109 25 8 28 83 57 36 88 44 94 13 48 43 93 70 31 79 120
     do
-        echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/version1/main.py -c -i $B -l $A -m uncorrelated >/home/rxp163130/scratch/Run42/$A.$B.out"
+        echo "python3 /home/rxp163130/QstarFromTidalSynchronization/MCMC/version1/main.py -c -i $B -l $A -m uncorrelated >/home/rxp163130/scratch/Run43/$A.$B.out"
     done
 done >uncorrelated
 
